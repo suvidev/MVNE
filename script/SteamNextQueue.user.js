104d840ecb75c83a96d723ee93c3e8a4623495dd
axezor <malcaninja@gmail.com>
2015-12-26 16:27:06 +0700

    ex1
    new script for steam 2015

---
+++ script/SteamNextQueue.user.js
@@ -0,0 +1,113 @@
+﻿// ==UserScript==
+// @name         Steam Next Queue
+// @namespace    http://axezor.blogspot.com
+// @version      0.2
+// @description  Enjoy :P
+// @author       AxEzOr
+// @match        http://store.steampowered.com/app/*
+// @match        http://store.steampowered.com/explore/
+// @match        http://store.steampowered.com/agecheck/app/*
+// @grant        none
+// @grant    GM_setValue
+// @grant    GM_getValue
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+
+//GM_setValue("autoNextSQ", false); 
+//GM_setValue("nowRoundSQ", 1);
+
+// Your code here...
+function getNowRound(){
+    
+    if(typeof(GM_getValue("nowRoundSQ")) === 'undefined'){
+        GM_setValue("nowRoundSQ", 0);
+    }
+    
+    return GM_getValue("nowRoundSQ");
+    
+}
+
+var autoNextSQ = GM_getValue("autoNextSQ");
+
+var nowRoundSQ = getNowRound();//parseInt(GM_getValue("nowRoundSQ"));
+
+
+function goNextSQ(){
+    if(autoNextSQ){
+
+        if(document.getElementById('ageYear')){
+            
+            document.getElementById('ageYear').value = '1988';
+            if(document.querySelector('a.btnv6_blue_hoverfade.btn_small')){
+                document.querySelector('a.btnv6_blue_hoverfade.btn_small').click();
+            }
+
+        }else{
+
+            if(document.querySelector('div.queue_actions_ctn div').onclick){
+
+
+                if(document.querySelector('div.queue_actions_ctn div span')){
+                    if(document.querySelector('div.queue_actions_ctn div span').textContent === 'Finish Queue'){
+                        GM_setValue("autoNextSQ", false); 
+                    }
+                }
+
+                document.querySelector('div.queue_actions_ctn div').click();
+
+            }else{
+                GM_setValue("autoNextSQ", false); 
+            }
+
+        }
+
+    }
+}
+
+if(autoNextSQ){
+    goNextSQ();
+}else{
+    
+    if(nowRoundSQ <= 2){
+        
+        if(document.getElementById('refresh_queue_btn')){
+            GM_setValue("autoNextSQ", true); 
+            GM_setValue("nowRoundSQ", parseInt(nowRoundSQ) + 1);
+            document.getElementById('refresh_queue_btn').click();
+        }
+        
+    }
+    
+}
+
+
+
+function genRunBot(){
+    var linkx = document.createElement("a");
+    linkx.href = "javascript:void(0)";
+    var linkText = document.createTextNode("Run");
+    linkx.appendChild(linkText);
+    linkx.id = "runstq";
+    linkx.style.position = "absolute";
+    linkx.style.top = "8px";
+    linkx.style.right = "50px";
+    linkx.style.cursor = "pointer";
+
+    document.body.appendChild(linkx);
+    document.getElementById('runstq').addEventListener('click', function() { 
+        GM_setValue("autoNextSQ", true); 
+        GM_setValue("nowRoundSQ", 1);
+        window.location.href = window.location.href;
+
+    });   
+}
+
+if(document.querySelector('div.queue_actions_ctn div') && !autoNextSQ){
+    if(document.querySelector('div.queue_actions_ctn div').onclick){
+        genRunBot();
+    }
+}
+    
+
