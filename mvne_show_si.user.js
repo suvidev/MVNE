ea43edd686e963245c126263bbb457c66c830d28
axezor <malcaninja@gmail.com>
2016-06-22 18:10:33 +0700

    Create mvne_show_si.user.js

---
+++ mvne_show_si.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         MVNE-SHOW-SI
+// @namespace    http://kormajudpai.blogspot.com
+// @version      0.1
+// @description  Enjoy :)
+// @author       AxEzOr
+// @match        http://bsspreprod.catmvne.co.th/*
+// @match        http://bssom.catmvne.co.th/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function genShowAccNo(){
+       // console.log('...b.');
+        //if(document.getElementById('accountinfolistbox-rows')){
+
+        if( document.querySelectorAll('td div a[id*="serviceinstancename"]').length > 0 ){
+
+            var ggArray = document.querySelectorAll('td div a[id*="serviceinstancename"]');
+
+            for(var i=0;i<ggArray.length;i++){
+
+                if(ggArray[i].textContent.indexOf('[[') === -1){
+
+                    var nTxt = ggArray[i].href+'';
+                    nTxt = nTxt.substring((nTxt.indexOf('c_strAccountNo=')+15));
+                    nTxt = nTxt.substring(0, (nTxt.indexOf('&')));
+
+                    ggArray[i].textContent = ggArray[i].textContent + ' [[' +nTxt+']]';
+
+                }else{
+                    break;
+                }
+
+            }
+
+        }
+        
+
+
+        //}
+
+    }
+
+    function loopingFunction1() {
+        //console.log('a....');
+        // if(!document.getElementById('fbvdlv2')){
+        genShowAccNo();  
+        //  }
+
+        //console.log('loop 1');
+        setTimeout(loopingFunction1, 3000);
+    }
+
+
+    loopingFunction1();
+
+
+
+
+
+
+})();
