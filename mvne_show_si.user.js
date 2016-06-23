addff73daf31256c69cad72ec6d975ad98707ca6
axezor <malcaninja@gmail.com>
2016-06-23 10:40:20 +0700

    Update mvne_show_si.user.js

--- mvne_show_si.user.js
+++ mvne_show_si.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MVNE-SHOW-SI
 // @namespace    http://kormajudpai.blogspot.com
-// @version      0.1
+// @version      0.2
 // @description  Enjoy :)
 // @author       AxEzOr
 // @match        http://bsspreprod.catmvne.co.th/*
@@ -32,6 +32,26 @@
                     nTxt = nTxt.substring(0, (nTxt.indexOf('&')));
 
                     ggArray[i].textContent = ggArray[i].textContent + ' [[' +nTxt+']]';
+
+                }else{
+                    break;
+                }
+
+            }
+
+        }else if( document.querySelectorAll('td div a[id*="planname_"]').length > 0 ){
+
+            var ggArray2 = document.querySelectorAll('td div a[id*="planname_"]');
+
+            for(var i2=0;i2<ggArray2.length;i2++){
+
+                if(ggArray2[i2].textContent.indexOf('[[') === -1){
+
+                    var nTxt2 = ggArray2[i2].href+'';
+                    nTxt2 = nTxt2.substring((nTxt2.indexOf('c_strAccountNo=')+15));
+                    nTxt2 = nTxt2.substring(0, (nTxt2.indexOf('&')));
+
+                    ggArray2[i2].textContent = ggArray2[i2].textContent + ' [[' +nTxt2+']]';
 
                 }else{
                     break;
