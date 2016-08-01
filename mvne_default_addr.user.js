53df3d1a12dff776f976e807741172549de97d47
suvidev <suvi.devteam@gmail.com>
2016-08-01 17:24:25 +0700

    Update mvne_default_addr.user.js

--- mvne_default_addr.user.js
+++ mvne_default_addr.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MVNE default address detail
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://crmpreprod.catmvne.co.th/index.php?module=Accounts&view=Edit
@@ -12,36 +12,43 @@
 
 (function() {
     'use strict';
-    
+
+    var toDo = true;
+
     function makeDefault(){
         var returnChk = false;
-        var tableArray = document.querySelectorAll('table tr th.blockHeader');
-        var tableAddr = null;
-        for(var i=0;i<tableArray.length;i++){
-            if("Address Details" === tableArray[i].textContent){
-                tableAddr = tableArray[i].parentNode.parentNode.parentNode;
-                break;
+
+        if(toDo){
+
+            var tableArray = document.querySelectorAll('table tr th.blockHeader');
+            var tableAddr = null;
+            for(var i=0;i<tableArray.length;i++){
+                if("Address Details" === tableArray[i].textContent){
+                    tableAddr = tableArray[i].parentNode.parentNode.parentNode;
+                    break;
+                }
+            }
+
+            if(tableAddr){
+
+                var fieldInputArray = tableAddr.querySelectorAll('input[data-fieldinfo*="mandatory"]');
+
+                for(var j=0;j<fieldInputArray.length;j++){
+                    fieldInputArray[j].value = '-';
+                    returnChk = true;
+                    toDo = false;
+                }
+
             }
         }
-        
-        if(tableAddr){
-            
-            var fieldInputArray = tableAddr.querySelectorAll('input[data-fieldinfo*="mandatory"]');
-            
-            for(var j=0;j<fieldInputArray.length;j++){
-                fieldInputArray[j].value = '-';
-                returnChk = true;
-            }
-            
-        }
-        
+
         return returnChk;
-        
+
     }
-    
-    
+
+
     if(!makeDefault()){
-        setInterval(function(){ makeDefault(); }, 3000);
+        setTimeout(makeDefault, 3000);
     }
 
     // Your code here...
